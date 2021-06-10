066caa2cdd79addf5b521de18f731585a59e3dce
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-10 08:39:29 -0400

    Queue built.

--- python/queue.py
+++ python/queue.py
@@ -1,14 +1,19 @@
 class Queue:
   # write your __init__ method here that should store a 'total' value which is the total number of elements in the Queue and a 'queue' value which is an array of stored values in the Queue
+    def __init__(self, total = 0, queue = []) -> None:
+        self.total = total
+        self.queue = queue
+            
+    def enqueue(self, data):
+        # write your code to add data to the Queue following FIFO and return the Queue
+        self.queue.append(data)
+        return self.queue
 
-  def enqueue(self):
-    # write your code to add data to the Queue following FIFO and return the Queue
-    pass
+    def dequeue(self, data):
+        # write your code to removes the data to the Queue following FIFO and return the Queue
+        self.queue.pop()
+        return self.queue
 
-  def dequeue(self, data):
-    # write your code to removes the data to the Queue following FIFO and return the Queue
-    pass
-
-  def size(self):
-    # write your code that returns the size of the Queue
-    pass
+    def size(self):
+        # write your code that returns the size of the Queue
+        return len(self.queue)
