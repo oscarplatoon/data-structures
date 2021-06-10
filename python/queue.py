f9213f500a45d91dfcaf5396fc6d064923b72b0b
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-10 08:48:08 -0400

    Stack working

--- python/queue.py
+++ python/queue.py
@@ -1,17 +1,21 @@
 class Queue:
   # write your __init__ method here that should store a 'total' value which is the total number of elements in the Queue and a 'queue' value which is an array of stored values in the Queue
-    def __init__(self, total = 0, queue = []) -> None:
-        self.total = total
+    def __init__(self, total = None, queue = []):
         self.queue = queue
+        if self.total == None:
+            self.total = len(self.queue)
+        self.total = total
             
     def enqueue(self, data):
         # write your code to add data to the Queue following FIFO and return the Queue
         self.queue.append(data)
+        self.total += 1
         return self.queue
 
     def dequeue(self, data):
         # write your code to removes the data to the Queue following FIFO and return the Queue
         self.queue.pop()
+        self.total -= 1
         return self.queue
 
     def size(self):
--- python/stack.py
+++ python/stack.py
@@ -1,14 +1,23 @@
 class Stack:
   # write your __init__ method here that should store a 'total' value which is the total number of elements in the Stack and a 'stack' value which is an array of stored values in the Stack
+    def __init__(self, stack = [], total = None):
+        self.stack = stack
+        if len(self.stack) > 0:
+            self.total = len(self.stack)
+        self.total = total
 
-  def push(self):
-    # write your code to add data following LIFO and return the Stack
-    pass
+    def push(self, data):
+        # write your code to add data following LIFO and return the Stack
+        self.stack.push(data)
+        self.total = len(self.stack)
+        return self.stack
+    
+    def pop(self, data):
+        # write your code to removes the data following LIFO and return the Stack
+        self.stack.pop(0)
+        self.total = len(self.stack)
+        return self.stack
 
-  def pop(self, data):
-    # write your code to removes the data following LIFO and return the Stack
-    pass
-
-  def size(self):
-    # write your code that returns the size of the Stack
-    pass
+    def size(self):
+        # write your code that returns the size of the Stack
+        return len(self.stack)
